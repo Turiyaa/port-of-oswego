2a37cb300ce04f35d7c3e61d8531aa35e6d26dda
turiyaa <nneopane@oswego.edu>
2020-04-15 16:06:46 -0400

    Updates

---
+++ precache-manifest.e8b5df77e453f4e2b14694b3e6768504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15eae90a7f00532f1bbf916e474b0848",
+    "url": "/port-of-oswego/index.html"
+  },
+  {
+    "revision": "74d574852fba70d6d2e0",
+    "url": "/port-of-oswego/static/css/main.ea4c5be8.chunk.css"
+  },
+  {
+    "revision": "257d751e122fca471a29",
+    "url": "/port-of-oswego/static/js/2.0e335897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/port-of-oswego/static/js/2.0e335897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d574852fba70d6d2e0",
+    "url": "/port-of-oswego/static/js/main.1c86c0f8.chunk.js"
+  },
+  {
+    "revision": "447d5ac7056140922f2b",
+    "url": "/port-of-oswego/static/js/runtime-main.52f1428e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/port-of-oswego/static/media/logo.5d5d9eef.svg"
+  }
+]);
